533b9b17756388ed899c4b4b74588e1b65e90168
Blackpaper13 <chris321.yusti@gmail.com>
2022-09-19 14:18:28 +0700

    GD 5 Live demo

---
+++ app/src/main/java/com/example/room_kelas_5digitnpm/room/NoteDao.kt
@@ -0,0 +1,30 @@
+package com.example.room_kelas_5digitnpm.room
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface NoteDao {
+
+
+    @Insert
+   suspend fun addNote(note: Note)
+
+
+    @Update
+    suspend fun updateNote(note: Note)
+
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+
+
+    @Query("SELECT * FROM note")
+    suspend fun getNotes() : List<Note>
+
+    @Query("SELECT * FROM note WHERE id =:note_id")
+    suspend fun getNote(note_id: Int) : List<Note>
+}
